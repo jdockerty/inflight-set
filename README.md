5622828aa0f34d5b82f34a6a462690e3a74a3e3f
Jack Dockerty <jdockerty19@gmail.com>
2026-05-29 08:12:37 +0100

    chore: rename

--- README.md
+++ README.md
@@ -1,23 +1,23 @@
-# keyed-semaphore
+# inflight-set
 
-Acquire many distinct permits based on a keyed name.
+Acquire many distinct guards based on a keyed name.
 
 This means that many RAII guards can be given out for _different_ keys, but only a single instance for the given key can be inflight at once.
 
 For example
 
 ```rust
-let s = KeyedSemaphore::new();
-let permit = s.acquire("job_id_123").expect("known unique key");
-let permit_two = s.acquire("job_id_567").expect("known unique key");
+let s = InflightSet::new();
+let guard = s.acquire("job_id_123").expect("known unique key");
+let guard_two = s.acquire("job_id_567").expect("known unique key");
 
 // do things
 
-// Would error! Attempting to acquire permit for a pre-existing key.
-let another_permit = s.acquire("job_id_123")?;
+// Would error! Attempting to acquire guard for a pre-existing key.
+let another_guard = s.acquire("job_id_123")?;
 
-drop(permit); // key=job_id_123
+drop(guard); // key=job_id_123
 
-let permit = s.acquire("job_id_123").expect("RAII guard dropped, this is okay");
-// When permits go out of scope, the key is released freeing it for later use.
+let guard = s.acquire("job_id_123").expect("RAII guard dropped, this is okay");
+// When guards go out of scope, the key is released freeing it for later use.
 ```
--- src/lib.rs
+++ src/lib.rs
@@ -1,23 +1,23 @@
-//!Acquire many distinct permits based on a keyed name.
+//!Acquire many distinct guards based on a keyed name.
 //!
 //!This means that many RAII guards can be given out for _different_ keys, but only a single instance for the given key can be inflight at once.
 //!
 //!For example
 //!
 //!```ignore
-//!let s = KeyedSemaphore::new();
-//!let permit = s.acquire("job_id_123").expect("known unique key");
-//!let permit_two = s.acquire("job_id_567").expect("known unique key");
+//!let s = InflightSet::new();
+//!let guard = s.acquire("job_id_123").expect("known unique key");
+//!let guard_two = s.acquire("job_id_567").expect("known unique key");
 //!
 //!// do things
 //!
-//!// Would error! Attempting to acquire permit for a pre-existing key.
-//!let another_permit = s.acquire("job_id_123")?;
+//!// Would error! Attempting to acquire guard for a pre-existing key.
+//!let another_guard = s.acquire("job_id_123")?;
 //!
-//!drop(permit); // key=job_id_123
+//!drop(guard); // key=job_id_123
 //!
-//!let permit = s.acquire("job_id_123").expect("RAII guard dropped, this is okay");
-//!// When permits go out of scope, the key is released freeing it for later use.
+//!let guard = s.acquire("job_id_123").expect("RAII guard dropped, this is okay");
+//!// When guards go out of scope, the key is released freeing it for later use.
 //!```
 
 use std::{collections::HashSet, sync::Arc};
@@ -26,63 +26,63 @@ use parking_lot::Mutex;
 use thiserror::Error;
 
 #[derive(Debug, Error)]
-pub enum SemaphoreError {
-    #[error("permit for {0} has already been given out")]
+pub enum InflightSetError {
+    #[error("guard for {0} has already been given out")]
     DuplicateKey(String),
 }
 
-/// RAII guard for the [`KeyedSemaphore`].
+/// RAII guard for the [`InflightSet`].
 ///
 /// When this is dropped, the key is freed from the
-/// overarching semaphore.
+/// overarching set.
 #[derive(Debug)]
-pub struct KeyedSemaphorePermit<'a> {
-    inner: &'a KeyedSemaphore,
+pub struct InflightGuard<'a> {
+    inner: &'a InflightSet,
     key: Arc<str>,
 }
 
-impl Drop for KeyedSemaphorePermit<'_> {
+impl Drop for InflightGuard<'_> {
     fn drop(&mut self) {
         self.inner.keys.lock().remove(&self.key);
     }
 }
 
-/// A semaphore implementation which allows for many distinct
-/// permits, keyed by name, to be given out at one time.
+/// A set implementation which allows for many distinct
+/// guards, keyed by name, to be given out at one time.
 ///
-/// However, requests to acquire a [`KeyedSemaphorePermit`] when
+/// However, requests to acquire an [`InflightGuard`] when
 /// a key is already in use will result in an error. It is
 /// a decision on the callee how to handle this.
 #[derive(Debug)]
-pub struct KeyedSemaphore {
+pub struct InflightSet {
     keys: Mutex<HashSet<Arc<str>>>,
 }
 
-impl Default for KeyedSemaphore {
+impl Default for InflightSet {
     fn default() -> Self {
         Self::new()
     }
 }
 
-impl KeyedSemaphore {
+impl InflightSet {
     pub fn new() -> Self {
         Self {
             keys: Mutex::new(HashSet::new()),
         }
     }
 
-    /// Acquire a [`KeyedSemaphorePermit`], assigning a key for the permit acquisition.
+    /// Acquire an [`InflightGuard`], assigning a key for the guard acquisition.
     ///
-    /// Future [`KeyedSemaphore::acquire`] calls which use the same key are rejected until
-    /// the permit is dropped.
-    pub fn acquire(&self, key: &str) -> Result<KeyedSemaphorePermit<'_>, SemaphoreError> {
+    /// Future [`InflightSet::acquire`] calls which use the same key are rejected until
+    /// the guard is dropped.
+    pub fn acquire(&self, key: &str) -> Result<InflightGuard<'_>, InflightSetError> {
         let key: Arc<str> = key.into();
 
         if !self.keys.lock().insert(Arc::clone(&key)) {
-            return Err(SemaphoreError::DuplicateKey(key.to_string()));
+            return Err(InflightSetError::DuplicateKey(key.to_string()));
         }
 
-        Ok(KeyedSemaphorePermit { inner: self, key })
+        Ok(InflightGuard { inner: self, key })
     }
 
     /// The number of active keys, yet to be dropped.
@@ -98,33 +98,33 @@ impl KeyedSemaphore {
 
 #[cfg(test)]
 mod test {
-    use crate::{KeyedSemaphore, SemaphoreError};
+    use crate::{InflightSet, InflightSetError};
 
     #[test]
     fn key_drop_semantics() {
-        let s = KeyedSemaphore::new();
+        let s = InflightSet::new();
         assert_eq!(s.len(), 0);
 
-        let permit = s.acquire("my_job_id").unwrap();
+        let guard = s.acquire("my_job_id").unwrap();
         assert_eq!(s.len(), 1);
-        drop(permit);
+        drop(guard);
         assert_eq!(s.len(), 0);
     }
 
     #[test]
     fn duplicate_key() {
-        let s = KeyedSemaphore::new();
+        let s = InflightSet::new();
 
-        let _permit = s.acquire("key_123").unwrap();
+        let _guard = s.acquire("key_123").unwrap();
         assert_eq!(s.len(), 1);
         assert!(matches!(
             s.acquire("key_123").unwrap_err(),
-            SemaphoreError::DuplicateKey(_)
+            InflightSetError::DuplicateKey(_)
         ));
         assert_eq!(
             s.len(),
             1,
-            "Permit rejection should not increase stored keys"
+            "Guard rejection should not increase stored keys"
         );
     }
 }
